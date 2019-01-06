c4a8fe273a3e8804d4e5a8f745acc84c8364a0f1
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-01-06 14:56:11 +0100

    Update autoinstallation.sh

--- Bash/autoinstallation.sh
+++ Bash/autoinstallation.sh
@@ -11,6 +11,39 @@ cNOR="\e[0m"
 cBLON="\e[5m" #Blinken Ein
 cBLOFF="\e[25m" #Blinken Aus
 
+
+# _____ _   _____________ _   _ _   _ _____ _____ _____ _____ _   _  _____ 
+#/  ___| | | | ___ \  ___| | | | \ | /  __ \_   _|_   _|  _  | \ | |/  ___|
+#\ `--.| | | | |_/ / |_  | | | |  \| | /  \/ | |   | | | | | |  \| |\ `--. 
+# `--. \ | | | ___ \  _| | | | | . ` | |     | |   | | | | | | . ` | `--. \
+#/\__/ / |_| | |_/ / |   | |_| | |\  | \__/\ | |  _| |_\ \_/ / |\  |/\__/ /
+#\____/ \___/\____/\_|    \___/\_| \_/\____/ \_/  \___/ \___/\_| \_/\____/
+
+function chownit {
+	echo -e "Ändere Zugriffsberechtigungen von $target"
+	chown $uname:$uname $target
+}
+
+function chmodit {
+	echo "$target wird Ausführbar gemacht"
+	chmod +x $target
+}
+
+function showerror {
+	echo -e "$name $cRED nicht vorhanden! $cNOR"
+}
+
+function showok {
+	echo -e "$name $cGREEN Vorhanden $cNOR"
+}
+
+
+#___  ___  ___  _____ _   _ ______ _   _ _   _ _____ _____ _____ _____ _   _  _____ 
+#|  \/  | / _ \|_   _| \ | ||  ___| | | | \ | /  __ \_   _|_   _|  _  | \ | |/  ___|
+#| .  . |/ /_\ \ | | |  \| || |_  | | | |  \| | /  \/ | |   | | | | | |  \| |\ `--. 
+#| |\/| ||  _  | | | | . ` ||  _| | | | | . ` | |     | |   | | | | | | . ` | `--. \
+#| |  | || | | |_| |_| |\  || |   | |_| | |\  | \__/\ | |  _| |_\ \_/ / |\  |/\__/ /
+#\_|  |_/\_| |_/\___/\_| \_/\_|    \___/\_| \_/\____/ \_/  \___/ \___/\_| \_/\____/ 
 #                                      _____             
 #                                     / __  \            
 # _ __  _ __ ___ _ __   __ _ _ __ ___ `' / /' __ _  ___  
@@ -22,11 +55,12 @@ cBLOFF="\e[25m" #Blinken Aus
 #Teste ob Script Rootberechtigung besitzt und ob Updates vorhanden sind
 function prepare2go {
 	#Überprüfe ob das Script Rootprivilegien besitzt.
+	name="Rootberechtigung"
 	if [ "$EUID" -ne 0 ]; then 
-		echo -e "Rootberechtigung $cRED Nein $cNOR"
+		showerror
 		sudo -i
 	else
-		echo -e "Rootberechtigung \e[92mOK\e[0m"
+		showok
 	fi
 
 	#Installiere vorhandene updates
@@ -93,13 +127,14 @@ function userisok {
 
 #Funktion zur Überprüfung der Privilegien des neuen Users
 function userisroot {
+	name="Rootberechtigung für den User $uname:"
 	sudocheck=$(cat /etc/sudoers | grep $uname | wc -l)
 	if (( $sudocheck < 1 )); then 
-		echo -e "$cRED ACHTUNG! $cNOR der angelegte User hat keine Root priviliegen!"
+		showerror
 		echo -e "$cRED Script wird geschlossen! $cNOR Bitte Rootrechte manuell eintragen mittels dem Befehl $cGREEN sudo visudo $cNOR. Dort unterhalb von $cRED root    ALL=(ALL:ALL) ALL $cNOR die Zeile $cGREEN $cBLON $uname    ALL=(ALL:ALL) ALL $cBLOFF $cNOR einfügen."
 		exit 1
 	else
-		echo -e "Rootberechtigung für den User $uname: $cGREEN OK $cNOR"
+		showok
 	fi
 }
 
@@ -148,11 +183,12 @@ function removepiuser {
 #                         |_|
 #Falls noch nicht vorhanden PiHole installieren
 function installpihole {
+	name="PiHole:"
 	checkpihole=$(ps ax | grep pihole | wc -l)
 	if (( $checkpihole > 1 )); then
-		echo -e "PiHole: $cGREEN Installiert $cNOR"
+		showok
 	else
-		echo -e "PiHole: $cRED Nicht Installiert $cNOR"
+		showerror
 		echo -e "$cGREEN PiHole $cNOR wird installiert"
 		curl -sSL https://install.pi-hole.net | bash
 		echo -e "$cGREEN Passwort $cNOR für PiHole Webclient eingeben. Leer lassen für kein Passwort"
@@ -171,11 +207,12 @@ function installpihole {
 #                         |_|           |_|  
 #Falls noch nicht vorhanden PiVPN installieren
 function installpivpn {
+	name="PiVPN:"
 	checkvpn=$(ps ax | grep openvpn | wc -l)
 	if (( $checkvpn > 1 )); then
-		echo -e "PiVPN: $cGREEN Installiert $cNOR"
+		showok
 	else
-		echo -e "PiVPN: $cRED Nicht Installiert $cNOR"
+		showerror
 		echo -e "PiVPN: Installation wird gestartet"
 		curl -L https://install.pivpn.io | bash
 		echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
@@ -200,21 +237,22 @@ function installpivpn {
 #                                 |_| 
 #Falls noch nicht vorhanden FTP Service installieren.
 function installftp {
+	name="FTP Dienst:"
+	target="$HOME/ftp"
 	checkftp=$(ps ax | grep ftp | wc -l)
 	if (( $checkftp < 1 )); then
-		echo -e "FTP Dienst: $cRED Nicht Installiert $cNOR" 
+		showerror
 		apt-get install vsftpd -y
 		echo -e "Entsprechende Ordner werden erstellt"
-		mkdir $HOME/ftp
-		mkdir $HOME/ftp/files
-		echo -e "Setze Berechtigungen für Ordner."
-		chown $uname:$uname $HOME/ftp
+		mkdir $target
+		mkdir $target/files
+		chownit
 		echo -e "Füge benötigte Einträge in vsftpd.conf ein"
 		echo "user_sub_token=$uname" >> /etc/vsftpd.conf
 		echo "local_root=/home/$uname/ftp" >> /etc/vsftpd.conf
 		service vsftpd restart
 	else
-		echo -e "FTP Dienst: $cGREEN Installiert $cNOR"
+		showok
 	fi
 		
 }
@@ -227,9 +265,10 @@ function installftp {
 #|_|_| |_|___/\__\__,_|_|_|\__,_|\__,_|\___|
 #Falls noch nicht vorhanden DUC für noip.com installieren. 
 function installduc {
+	name="DUC:"
 	checkduc=$(ps ax | grep noip | wc -l)
 	if (( $checkduc < 1 )); then 
-		echo -e "DUC: $cRED Nicht Installiert $cNOR"
+		showerror
 		cd /usr/local/src/
 		echo "Downloade DUC Files"
 		wget http://www.no-ip.com/client/linux/noip-duclinux.tar.gz
@@ -248,53 +287,60 @@ function installduc {
 		ducinitd
 		update-rc.d noip2 defaults
 	else
-		echo -e "DUC: $cGREEN Installiert $cNOR"
+		showok
 	fi
 }
 
 #Erstelle noip2 script in /etc/init.d/
 function ducinitd {
-	touch /etc/init.d/noip2
-	echo -e "Init.d Service wird eingerichtet"
-	echo '#! /bin/sh' >> /etc/init.d/noip2
-	echo '# /etc/init.d/noip2.sh' >> /etc/init.d/noip2
-	echo '### BEGIN INIT INFO' >> /etc/init.d/noip2
-	echo '# Provides: noip2' >> /etc/init.d/noip2
-	echo '# Required-Start: $remote_fs $local_fs' >> /etc/init.d/noip2
-	echo '# Required-Stop: $remote_fs $local_fs' >> /etc/init.d/noip2
-	echo '# Should-Start:' >> /etc/init.d/noip2
-	echo '# Should-Stop:' >> /etc/init.d/noip2
-	echo '# Default-Start: 2 3 4 5' >> /etc/init.d/noip2
-	echo '# Default-Stop: 0 1 6' >> /etc/init.d/noip2
-	echo '# Short-Description: Dynamic IP client updater' >> /etc/init.d/noip2
-	echo '# Description:' >> /etc/init.d/noip2
-	echo '### END INIT INFO' >> /etc/init.d/noip2
-	echo 'DAEMON=/usr/local/bin/noip2' >> /etc/init.d/noip2
-	echo 'NAME=noip2' >> /etc/init.d/noip2
-	echo 'test -x $DAEMON || exit 0' >> /etc/init.d/noip2
-	echo 'case "$1" in' >> /etc/init.d/noip2
-	echo '    start)' >> /etc/init.d/noip2
-	echo '    echo -n "Starting dynamic address update: "' >> /etc/init.d/noip2
-	echo '    start-stop-daemon --start --exec $DAEMON' >> /etc/init.d/noip2
-	echo '    echo "noip2."' >> /etc/init.d/noip2
-	echo '    ;;' >> /etc/init.d/noip2
-	echo '    stop)' >> /etc/init.d/noip2
-	echo '    echo -n "Shutting down dynamic address update:"' >> /etc/init.d/noip2
-	echo '    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON' >> /etc/init.d/noip2
-	echo '    echo "noip2."' >> /etc/init.d/noip2
-	echo '    ;;' >> /etc/init.d/noip2
-	echo '    restart)' >> /etc/init.d/noip2
-	echo '    echo -n "Restarting dynamic address update: "' >> /etc/init.d/noip2
-	echo '    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON' >> /etc/init.d/noip2
-	echo '    start-stop-daemon --start --exec $DAEMON' >> /etc/init.d/noip2
-	echo '    echo "noip2."' >> /etc/init.d/noip2
-	echo '    ;;' >> /etc/init.d/noip2
-	echo '    *)' >> /etc/init.d/noip2
-	echo '    echo "Usage: $0 {start|stop|restart}"' >> /etc/init.d/noip2
-	echo '    exit 1' >> /etc/init.d/noip2
-	echo 'esac' >> /etc/init.d/noip2
-	echo 'exit 0' >> /etc/init.d/noip2
-	chmod +x /etc/init.d/noip2
+	name="Init.d Eintrag für DUC:"
+	target="/etc/init.d/noip2"
+	if [ -e $target ]; then
+		showok
+	else
+		showerror	
+		touch $target
+		echo -e "Init.d Service wird eingerichtet"
+		echo '#! /bin/sh' >>$target
+		echo '# /etc/init.d/noip2.sh' >> $target
+		echo '### BEGIN INIT INFO' >> $target
+		echo '# Provides: noip2' >> $target
+		echo '# Required-Start: $remote_fs $local_fs' >> $target
+		echo '# Required-Stop: $remote_fs $local_fs' >> $target
+		echo '# Should-Start:' >> $target
+		echo '# Should-Stop:' >> $target
+		echo '# Default-Start: 2 3 4 5' >> $target
+		echo '# Default-Stop: 0 1 6' >> $target
+		echo '# Short-Description: Dynamic IP client updater' >> $target
+		echo '# Description:' >> $target
+		echo '### END INIT INFO' >> $target
+		echo 'DAEMON=/usr/local/bin/noip2' >> $target
+		echo 'NAME=noip2' >> $target
+		echo 'test -x $DAEMON || exit 0' >> $target
+		echo 'case "$1" in' >> $target
+		echo '    start)' >> $target
+		echo '    echo -n "Starting dynamic address update: "' >> $target
+		echo '    start-stop-daemon --start --exec $DAEMON' >> $target
+		echo '    echo "noip2."' >> $target
+		echo '    ;;' >> $target
+		echo '    stop)' >> $target
+		echo '    echo -n "Shutting down dynamic address update:"' >> $target
+		echo '    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON' >> $target
+		echo '    echo "noip2."' >> $target
+		echo '    ;;' >> $target
+		echo '    restart)' >> $target
+		echo '    echo -n "Restarting dynamic address update: "' >> $target
+		echo '    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON' >> $target
+		echo '    start-stop-daemon --start --exec $DAEMON' >> $target
+		echo '    echo "noip2."' >> $target
+		echo '    ;;' >> $target
+		echo '    *)' >> $target
+		echo '    echo "Usage: $0 {start|stop|restart}"' >> $target
+		echo '    exit 1' >> $target
+		echo 'esac' >> $target
+		echo 'exit 0' >> $target
+		chmodit
+	fi
 }
 
 # _           _        _ _  __ _____  _     
@@ -305,8 +351,9 @@ function ducinitd {
 #|_|_| |_|___/\__\__,_|_|_|_| \_____/|_.__/
 function installf2b {
 	checkf2b=$(ps ax | grep fail2ban | wc -l)
+	name="Fail2Ban:"
 	if (( $checkf2b < 1 )); then
-		echo -e "Fail2Ban: $cRED Nicht Installiert $cNOR"
+		showerror
 		echo "Installiere Fail2Ban"
 		apt-get install fail2ban -y
 		cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
@@ -316,7 +363,7 @@ function installf2b {
 		nano /etc/fail2ban/jail.local
 		systemctl restart fail2ban.service
 	else
-		echo -e "Fail2Ban: $cGREEN Installiert $cNOR"
+		showok
 	fi	
 }
 
@@ -360,7 +407,265 @@ function abschluss {
 	fi
 	echo "Enter zum fortfahren"
 	clear 
-	echo -e "$cGREEN Die Installation ist abgeschlossen $cNOR"
+	echo -e "$cGREEN Die Installation ist abgeschlossen. Starte Konfiguration $cNOR"
+}
+
+#                  __ _           _          __  __ 
+#                 / _(_)         | |        / _|/ _|
+#  ___ ___  _ __ | |_ _  __ _ ___| |_ _   _| |_| |_ 
+# / __/ _ \| '_ \|  _| |/ _` / __| __| | | |  _|  _|
+#| (_| (_) | | | | | | | (_| \__ \ |_| |_| | | | |  
+# \___\___/|_| |_|_| |_|\__, |___/\__|\__,_|_| |_|  
+#                        __/ |                      
+#                       |___/  
+#Erstelle für die Scripts benötigte Dateien und Ordner
+function configstuff {
+	name="Scriptsordner:"
+	target="$HOME/scripts"
+	#Prüfe vorhandensein des Scriptordners
+	if [ -d $target; then
+		showok
+	else
+		showerror
+		echo -e "Scriptordner wird angelegt unter $target"
+		mkdir $target
+		chownit
+	fi
+
+	#Prüfe vorhandensein von telegram.inf
+	name="Telegram.inf:"
+	target="$HOME/scripts/telegram.inf"
+	if [ -e $target ]; then
+		showok
+	else
+		showerror
+		touch $target
+		chownit
+		echo "BOT_ID=" >> $target
+		echo "CHAT_ID=" >> $target
+	fi
+
+	#Prüfe vorhandensein von user.inf
+	name="User.inf:"
+	target="$HOME/scripts/user.inf"
+	if [ -e $target ]
+		showok
+	else
+		showerror
+		echo -e "User.inf wird unter $target erstellt."
+		touch $target
+		chownit
+		echo "user=$uname" >> $target
+	fi
+}
+
+
+#            _                 _       _       
+#           | |               (_)     | |      
+#  __ _  ___| |_ ___  ___ _ __ _ _ __ | |_ ___ 
+# / _` |/ _ \ __/ __|/ __| '__| | '_ \| __/ __|
+#| (_| |  __/ |_\__ \ (__| |  | | |_) | |_\__ \
+# \__, |\___|\__|___/\___|_|  |_| .__/ \__|___/
+#  __/ |                        | |            
+# |___/                         |_|
+#Downloade gewünschte Scripts
+function getscripts {
+	cd $HOME/scripts
+	cmd=(dialog --separate-output --checklist "Wähle zu verwendende Scripts aus:" 22 76 16)
+	options=(1 "Boot Benachrichtigung" off    # any option can be set to default to "on"
+			 2 "Updatecheck" off
+			 3 "VPN Zertifikaterneuerung" off
+			 4 "Benachrichtigung wenn Mail von noip eintrifft" off
+			 5 "Benachricgtigung wenn öffentliche IP wechselt" off
+			 6 "Benachrichtigung bei Neujahr" off
+			 7 "Benachrichtigung bei eingehender VPN Verbindung" off
+			 8 "Benachrichtigung wenn Fail2Ban eine IP blockt/entblockt" off
+			 9 "Command & Control Script (slave)" off
+			 10 "Logcleaner" off)
+	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+	clear
+	for choice in $choices
+	do
+		case $choice in
+			1)
+				target="$HOME/scripts/startup.sh"
+				name="startup.sh:"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Bootbenachrichtigungsscript wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/startup.sh
+					chownit
+					chmodit
+				fi
+				;;
+			2)
+				name="Updatecheck:"
+				target="$HOME/scripts/updatecheck.sh"
+				if [ -e $target ]: then
+					showok
+				else
+					showerror
+					echo "Updatecheck wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/updatecheck.sh
+					chownit
+					chmodit
+				fi
+				;;
+			3)
+				name="VPN Zertifikatserneuerungsscript:"
+				target="$HOME/scripts/VPN_renew_server_cert.sh"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "VPN Zertifikatserneuerungsscript wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/VPN_renew_server_cert.sh
+					chownit
+					chmodit
+				fi
+				;;
+			4)
+				name="Checkpublicip:"
+				target="$HOME/scripts/checkpublicip.sh"				
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Script zur überprüfung der öffentlichen IP wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/checkpublicip.sh
+					chownit
+					chmodit
+				fi
+				;;
+			5)
+				name="Checkmail:"
+				target="$HOME/scripts/checkmail2.py"				
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Benachrichtigungsscript für eingehende Mails von Noip wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Python/checkmail2.py
+					chownit
+					chmodit
+				fi
+				;;
+			6)
+				name="Happy New Year Script:"
+				target="$HOME/scripts/Happy_New_Year.sh"				
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Happy New Year Script wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/Happy_New_Year.sh
+					chownit
+					chmodit
+				fi
+				;;
+			7)
+				name="VPNconnect:"
+				target="$HOME/scripts/VPNconnect.sh"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Benachrichtigungsscript für eingehende VPN-Verbindungen wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/VPNconnect.sh
+					chownit
+					chmodit
+				fi
+				;;
+			8)
+				name="Fail2Ban Benachrichtigungen:"
+				target="$HOME/scripts/f2b_info.sh"
+				if [ -e $target ]; then
+					showok
+				else	
+					showerror
+					echo "Benachrichtigungsscript für Fail2Ban wird gedownloaded"
+					wget https://github.com/Apop85/Scripts/blob/master/Bash/f2b_info.sh
+					chownit
+					chmodit
+				fi
+				;;
+			9)
+				name="Remote Slave Ordner:"
+				target="$HOME/scripts/remote"
+				if [ -d $target ]; then
+					showok
+				else
+					showerror
+					echo -e "Erstelle remote Ordner $target"
+					mkdir $target
+					chownit
+				fi
+				cd $target
+				
+				name="Remote Slave:"
+				target="$HOME/scripts/remote/remote_slave.sh"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Command & Controlscript (slave) wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/Remote_Script/remote_slave.sh
+					chownit
+					chmodit
+				fi
+				
+				echo "Übrige Remotescripts werden gedownloaded."
+				
+				name="remote_reboot:"
+				target="$HOME/scripts/remote/remote_reboot.sh"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo -e "Downloade $name"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/Remote_Script/remote_reboot.sh
+					chmodit
+				fi
+				
+				name="remote_update"
+				target="$HOME/scripts/remote/remote_update.sh"
+				if [ -e $target ]; then 
+					showok
+				else
+					showerror
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/Remote_Script/remote_update.sh
+					chmodit
+				fi
+				
+				name="updateclient"
+				target="$HOME/scripts/remote/updateclient.sh"
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/Remote_Script/updateclient.sh
+					chownit
+					chmodit
+				fi
+				cd $HOME/scripts
+				;;
+			10)
+				name="Logcleaner"
+				target="$HOME/scripts/logcleaner.sh"				
+				if [ -e $target ]; then
+					showok
+				else
+					showerror
+					echo "Logcleaner wird gedownloaded"
+					wget https://raw.githubusercontent.com/Apop85/Scripts/master/Bash/logcleaner.sh
+					chownit
+					chmodit
+				fi
+				;;
+		esac
+	done
 }
 
 # _____ _   _______   ___________  ______ _   _ _   _ _____ _____ _____ _____ _   _  _____ 
@@ -384,5 +689,7 @@ installpivpn
 installduc
 installf2b
 abschluss
+configstuff
+getscripts
 
 exit 0

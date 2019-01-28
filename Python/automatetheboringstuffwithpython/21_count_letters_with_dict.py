4b0c981b3021265064c6420a04ad9bf852414896
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-01-28 15:20:40 +0100

    Add files via upload

---
+++ Python/automatetheboringstuffwithpython/21_count_letters_with_dict.py
@@ -0,0 +1,23 @@
+print('Gebe einen beliebigen Satz oder eine beliebige Zeichenfolge ein')
+string=input()
+
+dict1={}
+
+for i in string:
+        dict1.setdefault(i, 0)
+        dict1[i]+=1
+
+while True:
+        print('Sortierte (0) oder normale (1) ausgabe?')
+        inp=int(input())
+        if inp == 0:
+                from pprint import pprint as pprint
+                pprint(dict1)
+                break
+        elif inp ==1:
+                for i, k in dict1.items():
+                    print('[\'' + str(i) + '\'] = [\'' + str(k) + '\']')
+                break
+        else:
+                print('1 oder 0, so schwer ist das nicht!')
+                continue

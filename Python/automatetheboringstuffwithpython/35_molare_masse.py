d75a5dad91fa1ba20a7c4a1270907b52e3e05aca
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-01-29 05:19:27 +0100

    Update 35_molare_masse.py

--- Python/automatetheboringstuffwithpython/35_molare_masse.py
+++ Python/automatetheboringstuffwithpython/35_molare_masse.py
@@ -61,4 +61,4 @@ try:
 
     print('Die molare Masse von', ''.join(sformel), 'ist', str(round(gew, 4)) + 'g/mol')
 except NameError:
-    print('Summenformel falsch formuliert. Beispiel: CO2, H2O, C9H8O4')
+    print('Summenformel falsch formuliert. Beispiel: CO2, H2O, C9H8O4, CaHPO4')

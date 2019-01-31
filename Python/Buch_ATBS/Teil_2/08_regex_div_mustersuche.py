13510a26827ba6de8b335e8a5f029b75716bd493
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-01-31 02:41:11 +0100

    changes and new stuff

--- Python/Buch_ATBS/Teil_2/08_regex_div_mustersuche.py
+++ Python/Buch_ATBS/Teil_2/08_regex_div_mustersuche.py
@@ -1,108 +1,154 @@
 # Uterschiedliche Prüfmuster für Regex
 import re
-from time import sleep
 delay=0.3
 
-def exreg():
-    for i in range(len(string)-withc+1):
-        chunk=string[i:i+withc]
-        ergebnis=suchnach.search(chunk)
-
-        if ergebnis != None:
-            sleep(delay)
-            print(ergebnis.group(), end=' ')
-            
 string=('Batman, Batwoman, Batwowoman.')
-print('Der Teststring lautet:')
-print(string)
-input()
+
+def foobar():
+    print(''.center(50, '█'))
 
 # ('Bat(wo)?man') Dieser Ausdruck sucht nach Batman. Zwischen Bat und man kann jedoch auch ein wo sein.
 print('Suche nach (Bat(wo)?man). Heisst das "wo" in Batwoman \nkann vorkommen, muss jedoch nicht. \nAuch Batman ist in diesem Fall Gültig')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'Bat(wo)?man')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
 # ('Batwo(ma)*n') Sucht nach dem Ausdruck Batwoman wobei das ma mehrmals vorkommen kann.
 print('Suche nach (Bat(wo)*man). Heisst das "wo" in Batwoman \nkann mehrmals vorkommen, muss jedoch nicht')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'Bat(wo)*man')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
 # ('Batwo(ma)+n') ist ein + angegeben heisst dass das Element muss mindestens 1x vorkommen
 print('Suche nach (Bat(wo)+man). Heisst dass "wo" in Batwoman \nmindestens einmal vorkommen muss')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'Bat(wo)+man')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
 # ('(Batwo(ma){2})+n') ist ein + angegeben heisst dass das Element muss mindestens 2x vorkommen
 print('Suche nach ((Bat(wo){2})+man). Heisst das "wo" in \nBatwoman muss mindestens 2x vorkommen')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'(Bat(wo){2})+man')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
 # findall() gibt alle übereinstimmenden Werte eines Strings aus.
-print('Alle bisherigen suchen wurden mit search() ausgeführt.\nDadurch wird nur immer die erste Übereinstimmung\n ausgegeben und der Rest wird ignoriert.\n Mit der funkntion findall() werden alle \nÜbereinstimmungen in einem String als Liste ausgelesen')
-print('Suchergebnis in:', string)
+print('Alle bisherigen suchen wurden mit search() ausgeführt.\nDadurch wird nur immer die erste Übereinstimmung\nausgegeben und der Rest wird ignoriert.\nMit der funkntion findall() werden alle \nÜbereinstimmungen in einem String als Liste ausgelesen')
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'(Bat)(wo)*(man)')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.findall(string)
 if ergebnis != False:
     print((''.join(ergebnis[0])),(''.join(ergebnis[1])), (''.join(ergebnis[2])))
-print()
+foobar()
 input()
 
 # Man kann auch ein Muster überprüfen von welchem man den genauen Aufbau nicht kennt.
 print('Mit ((\\w+)\\W+) wird nach einem String gesucht der \naus ainer unbekannten Anzahl Buchstaben besteht \nund mit einem Satzzeichen auffhört')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'(\w+)\W+')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.findall(string)
 if ergebnis != False:
     print(' '.join(ergebnis))
-print()
+foobar()
+input()
+
+
+# Man kann auch direkt nach einzelnen Buchstaben bzw Buchstabengruppen suchen
+print('Man kann mit (r\'[Bmw]\') auch Definieren welche \nZeichen gesucht werden soll.')
+foobar()
+print('Ergebnis in:', string)
+foobar()
+suchmuster=(r'[Bmw]')
+suchnach=re.compile(suchmuster)
+ergebnis=suchnach.findall(string)
+if ergebnis != False:
+    print(ergebnis)
+foobar()
+input()
+
+# Man kann auch Zeichensequenzen angeben nach welchen gesucht werden soll.
+print('Man kann mit (r\'[a-bA-D0-9]\') auch Definieren welche \nZeichensequenzen gesucht sind.\n Stellt man noch ein "^" vor die Zeichenkette\n Heisst dass diese Zeichen/Buchstaben nicht vorkommen \ndürfen es bedeutet also NOT')
+foobar()
+print('Ergebnis in:', string)
+foobar()
+suchmuster=(r'[a-bA-D0-9]')
+suchnach=re.compile(suchmuster)
+ergebnis=suchnach.findall(string)
+if ergebnis != False:
+    print(ergebnis)
+foobar()
 input()
 
 # Regex unterscheidet gierige von nicht gierigen Anweisungen. Bei einer gierigen Anweisung wird stets der grösstmögliche String gesucht
 string='HaHaHaHaHaHaHa'
 print('Suche nach (Ha{3,5}?). Sucht einen String in \nwelchem "Ha" 5 bis drei mal vorkommt. Durch das \nFragezeichen wird 3 bevorzugt. Man sagt auch dass \ndiese Anweisung nicht gierig ist.')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'(Ha){3,5}?')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
 
 print('Suche nach (Ha{3,5}). Sucht einen String in \nwelchem "Ha" 5 bis drei mal vorkommt. Durch das fehlen\ndes Fragezeichen wird 5 bevorzugt. Man sagt auch dass \ndiese Anweisung gierig ist.')
-print('Suchergebnis in:', string)
+foobar()
+print('Ergebnis in:', string)
+foobar()
 suchmuster=(r'(Ha){3,5}')
 suchnach=re.compile(suchmuster)
 ergebnis=suchnach.search(string)
 if ergebnis != False:
     print(ergebnis.group())
-print()
+foobar()
 input()
 
+# Mit dem Joker kann man nach unbekannten Sequenzen suchen.
+string='Hey, wie gehts dir? Danke! Es geht. Gut?'
+print('Das (*) in (\'\w(.*)[?]) ist der Joker. Wenn man\nNicht weiss wonach man genau sucht kann\ndieser verwendet werden. Hier werden\nBeispielsweise Fragen gesucht. Der Joker\nist gierig und sucht immer die grösste mögliche\nübereinstimmung. Dirch die Angabe von (.*?) kann\nman jedoch nach einem möglichst kleinen Treffer suchen')
+foobar()
+print('Ergebnis in:', string)
+foobar()
+suchmuster=(r'\w((.*)[?])[^!.:;]')
+suchnach=re.compile(suchmuster)
+ergebnis=suchnach.search(string)
+if ergebnis != False:
+    print(ergebnis.group())
+foobar()
+input()
---
+++ Python/Buch_ATBS/Teil_2/09_fragen_saetze_mit_regex.py
@@ -0,0 +1,43 @@
+# Finde Fragen und Sätze in einer Geschichte
+import re
+story='''My father died the day I was born. I don’t "believe" in ghosts or anything, but I think I could adjust to the idea pretty quickly; that’s what it felt like "only hearing" about someone that helped make you. 45 years later, my mother dies too. We have to clean out the old house before we sell it, and while I’m emptying their bedroom, I stub my toe on a piece of trim bumped out from the wall; I push the trim back into place, and a whole piece of the wall moves. By the time I’m done, I’ve probably pulled out a hundred sheets of paper; letters, notes to himself, little stories, and pictures — so many pictures. They all had his name on them, all in the same tight, narrow handwriting. I think I was confused more than anything else, I wasn’t choked up, just sitting there wondering why I’d never seen any of this before. Mom had stopped talking about him more recently, but she was just going at that point. When I was younger, I always heard about how everyone loved him, how good his cooking was, how funny he was. Honestly, once I saw the pictures, I half-expected to see another woman, maybe a whole other family. Funny thing was, they weren’t even of him. As best I could tell, they were of random people, just people he saw on the street, with written notes on the back like, “Man waiting outside restaurant for someone to call him back.” Really benign stuff, actually, but the pictures were beautiful. They all seemed a little worn, dirty. Not the physical pictures, but the images, "the people and places", like they all had been forgotten too. Just why? Why did this happen?'''
+
+# Suche nach Fragen
+suchmuster=(r"[a-zA-z ’']+[?]")
+suchnach=re.compile(suchmuster)
+fragen=suchnach.findall(story)
+frage=suchnach.search(story)
+print(''.center(50, '▼'))
+print('Anzahl Fragen:'.ljust(15), len(fragen))
+print('Erste Frage:'.ljust(15), (frage.group()).strip())
+print(''.center(50, '▲'))
+
+# Suche nach Sätzen
+suchmuster=(r"\w[a-zA-z ’']+[.!:;]")
+suchnach=re.compile(suchmuster)
+satz=suchnach.findall(story)
+sat=suchnach.search(story)
+print(''.center(50, '▼'))
+print('Anzahl Sätze:'.ljust(15), len(satz), '+', len(fragen))
+print('Erster Satz:'.ljust(15), (sat.group()).strip())
+print(''.center(50, '▲'))
+
+# Suche nach Zitaten
+suchmuster=(r'"(.*?)"')
+suchnach=re.compile(suchmuster)
+zitate=suchnach.findall(story)
+zitat=suchnach.search(story)
+print(''.center(50, '▼'))
+print('Anzahl Zitate:'.ljust(15), len(zitate))
+print('Erstes Zitat:'.ljust(15), (zitat.group()).strip())
+print(''.center(50, '▲'))
+
+# Suche nach Wörtern
+suchmuster=(r"[a-zA-Z’']+")
+suchnach=re.compile(suchmuster)
+wort=suchnach.findall(story)
+wor=suchnach.search(story)
+print(''.center(50, '▼'))
+print('Anzahl Wörter:'.ljust(15), len(wort))
+print('Erstes Wort:'.ljust(15), (wor.group()).strip())
+print(''.center(50, '▲'))

888b32359ec9d1f3aae69a158da811fa2132e8a5
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-02-08 14:15:52 +0100

    ...

--- Python/Buch_ATBS/Teil_2/Kapitel_12_Arbeiten_mit_Excel-Sheets/05_textformatierung_in_zellen/05_textformatierung_in_zellen.py
+++ Python/Buch_ATBS/Teil_2/Kapitel_12_Arbeiten_mit_Excel-Sheets/05_textformatierung_in_zellen/05_textformatierung_in_zellen.py
@@ -14,6 +14,7 @@ if os.path.exists(target_file):
 
 cleaned_cloud, word_cloud={}, {}
 
+# Zähle vorkommen der einzelnen Wörter innerhalb des angegebenen Files
 read_file=open(source_file, 'r', encoding='UTF-8')
 source_file_content=read_file.read()
 read_file.close()
@@ -28,16 +29,21 @@ for eintrag in word_cloud:
     if word_cloud[eintrag] > 12:
         cleaned_cloud.setdefault(eintrag, word_cloud[eintrag])
 
+# Bereite Dokument zum schreiben vor
 excel_sheet=openpyxl.Workbook()
 active_sheet=excel_sheet.active
-active_sheet.title = 'Word-Cloud'
+active_sheet.freeze_panes='A2'
 
+# Diverse Möglichkeiten zur anpassung der Schrift sowie Ausrichtung und Aussehen des Textes mit dem Modul Font
+active_sheet.title = 'Word-Cloud'
 sheet_title='Wordcloud des Dokuments '+source_file
 active_sheet.cell(row=1, column=1, value=sheet_title).font=Font(size=20, bold=True, underline='single', name='Times New Roman')
 active_sheet.row_dimensions[1].height=22
 
 countinue, row, colums=True, 2, 0
 max_colums=8
+
+# Setze Titel im und vom Abeitsblatt
 merge_range='A1:'+str(openpyxl.utils.get_column_letter(max_colums)+'1')
 active_sheet.merge_cells(merge_range)
 max_column_with={}
@@ -50,6 +56,7 @@ while countinue:
             break
         content=list(cleaned_cloud.keys())[i+colums]
         max_column_with.setdefault(openpyxl.utils.get_column_letter(i), 0)
+        # Merke Zahl für Spaltenbreite und höhe der Reihe
         if max_row_size < cleaned_cloud[content]:
             max_row_size=cleaned_cloud[content]
         if max_column_with[openpyxl.utils.get_column_letter(i)] < len(content):
@@ -57,18 +64,14 @@ while countinue:
         # Zelleneintrag mit der Schriftgrösse = Anzahl des Wortes
         active_sheet.cell(row=row, column=i, value=content).font=Font(size=cleaned_cloud[content])
         active_sheet.cell(row=row, column=i).alignment=Alignment(horizontal='center')
-        # active_sheet.column_dimensions[openpyxl.utils.get_column_letter(colums+i)].width=len(content*cleaned_cloud[content])
+    # Seitze höhe der Reihe auf die Anzahl des meistvorkommenden Wortes (= Schriftgrösse)
     active_sheet.row_dimensions[row].height=max_row_size
     colums+=10
 
+# Passe Spaltenbreite dem längsten Wort + Anzahl des vorkommenden Wortes // 2.2 an
 position=1
 for colum in max_column_with:
-    # word=
-    # word_amount=
     active_sheet.column_dimensions[colum].width=max_column_with[colum]+cleaned_cloud[list(cleaned_cloud.keys())[position]]//2.2
     position+=1
 
 excel_sheet.save(target_file)
-
-# for i in range(len(word_cloud)):
-#     active_sheet.cell(row=)
---
+++ Python/Buch_ATBS/Teil_2/Kapitel_12_Arbeiten_mit_Excel-Sheets/06_erstellen_von_diagrammen/06_erstellen_von_diagrammen.py
@@ -0,0 +1,6 @@
+# 06_erstellen_von_diagrammen.py
+# In diesem Beispiel geht es darum automatisch Diagramme in ein Excelsheet einzufügen.
+
+import os, openpyxl
+from openpyxl.styles import Font, Alignment
+

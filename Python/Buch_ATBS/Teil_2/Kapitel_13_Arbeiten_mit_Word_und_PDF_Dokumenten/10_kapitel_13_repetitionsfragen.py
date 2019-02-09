198cf2d2d480c623c4833932017b1415a32f65e2
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-02-09 15:46:30 +0100

    end of chapter 13

--- Python/Buch_ATBS/Teil_2/Kapitel_13_Arbeiten_mit_Word_und_PDF_Dokumenten/10_kapitel_13_repetitionsfragen.py
+++ Python/Buch_ATBS/Teil_2/Kapitel_13_Arbeiten_mit_Word_und_PDF_Dokumenten/10_kapitel_13_repetitionsfragen.py
@@ -17,44 +17,44 @@ def output(title, string):
     print('╚'+''.center(max_text_length+8, '═')+'╝')
     input()
 
-output('Frage 01', '')
-output('Antwort', '')
+output('Frage 01', 'Der Funktion PyPDF2.PdfFileReader() übergeben sie nicht den Stringwert mit dem Namen der PDF-Datei. Was übergeben sie statdessen?')
+output('Antwort', 'Man übergibt die Variabel welche die geöffnete Datei im binary Modus beinhaltet. Bsp: pdf_file_content = PyPDF2.PdfFileReader(open(pdf_file.pdf, "rb"))')
 
-output('Frage 02', '')
-output('Antwort', '')
+output('Frage 02', 'In welchen Modi müssen File-Objekte für PdfFileReader() und PdfFileWriter() geöffnet sein?')
+output('Antwort', 'Read Binary bzw "rb" Beispiel: open(pdf_file.pdf, "rb") oder im Modus Write-Binary für .PdfFileWriter()')
 
-output('Frage 03', '')
-output('Antwort', '')
+output('Frage 03', 'Wie rufen sie dsa Page-Objekt für die Seite 5 von einem PdfFileReader-Objekt ab?')
+output('Antwort', 'Man nutzt page_5=pdf_file_content.getPage(4) wobei ein PDF-Dokument immer mit der Seitenzahl 0 beginnt und daher getPage(4) für die 5. Seite verwendet werden muss.')
 
-output('Frage 04', '')
-output('Antwort', '')
+output('Frage 04', 'In welcher PdfFileReader-Variable ist die Anzahl der Seiten in einem PDF-Dokument gespeichert?')
+output('Antwort', 'pdf_file_content.numPages liest die Anzahl Seiten des PDF\'s aus')
 
-output('Frage 05', '')
-output('Antwort', '')
+output('Frage 05', 'Was müssen sie tun, bevor sie die Page-Objekte von einem PdfFileReader-Objekt abrufen können, dessen PDF mit dem Passwort "swordfish" geschützt ist?')
+output('Antwort', 'Mit pdf_file_content.decrypt("swordfish") lässt sich die Datei entschlüsseln. Mit .encrypt(passwort) wieder verschlüsseln')
 
-output('Frage 06', '')
-output('Antwort', '')
+output('Frage 06', 'Welche Methoden verwenden sie, um eine Seite zu drehen?')
+output('Antwort', 'Die Seite lässt sich mit page_5.rotateClockwise(90) drehen.')
 
-output('Frage 07', '')
-output('Antwort', '')
+output('Frage 07', 'Welche Methode gibt ein Document-Objekt für die Datei demo.docx zurück?')
+output('Antwort', 'Mit doc_file=docx.Document("demo.docx") lässt sich demo.docx auslesen und in einer Variabel speichern.')
 
-output('Frage 08', '')
-output('Antwort', '')
+output('Frage 08', 'Was ist der Unterschied zwischen einem Paragraphen- und einem Run-Objekt?')
+output('Antwort', 'Die Paragraphen beinhalten den Kompletten Text bis zum nächsten Zeilenumbruch und ist selber wiederum in Runs unterteilt welche das Aussehen der Textabschnitte bestimmt. Jedes mal wen sich die Formatierung ändert entsteht ein neuer Run.')
 
-output('Frage 09', '')
-output('Antwort', '')
+output('Frage 09', 'Wie rufen sie die Liste der Paragraphen-Objekte für ein Dokument ab das in der Variabel "doc" gespeichert ist?')
+output('Antwort', 'Die Funktion doc.paragraphs gibt alle Paragraphen-Objekte als Liste aus')
 
-output('Frage 10', '')
-output('Antwort', '')
+output('Frage 10', 'Was für Objekte verfügen über die Variablen bold, underline, italic, strike und outline?')
+output('Antwort', 'Diese Variablen gehören zum Run-Objekt und definieren ob der Text fett, unterstrichen, schräg, durchgestrichen oder outlined ist. Beispiel: run_objekt.italic=True')
 
-output('Frage 11', '')
-output('Antwort', '')
+output('Frage 11', 'Was ist der Unterschied zwischen den Werten True, False und None für die Variable bold?')
+output('Antwort', 'bold=True heisst dass der Text fett geschrieben wird, bold=False heisst er wird nicht Fett dargestellt und bold=None verwendet die Standardwerte des Run-Objekts')
 
-output('Frage 12', '')
-output('Antwort', '')
+output('Frage 12', 'Wie erstellen sie ein Document-Objekt für ein neues Word_Dokument?')
+output('Antwort', 'Ein neues Dokument kann man mit docx.Document() erstellen')
 
-output('Frage 13', '')
-output('Antwort', '')
+output('Frage 13', 'Wie fügen sie einem Document-Objekt in der Variablen doc einen Absatz mit dem Text "Hello there" hinzu?')
+output('Antwort', 'Mit doc.add_paragraph("Hello there") lässt sich ein neuer Abschnitt mit dem entsprechenden Textinhalt erstellen.')
 
-output('Frage 14', '')
-output('Antwort', '')
+output('Frage 14', 'Welche Integerwerte können sie verwenden, um in einem Word-Dokument Überschriften-Ebenen anzugeben?')
+output('Antwort', 'Mit doc_file.add_heading("Header Text", 0-4) lassen sich Header einfügen.')
---
+++ Python/Buch_ATBS/Teil_2/Kapitel_13_Arbeiten_mit_Word_und_PDF_Dokumenten/11_brute_force_pdf/11_brute_force_pdf.py
@@ -0,0 +1,129 @@
+# 11_brute_force_pdf.py
+# In diesem Übungsbeispiel geht es darum ein verschlüsseltes PDF zu knacken mittels der
+# Dictionary-Attack- oder Brute-Force-Methode
+
+import os, PyPDF2
+os.chdir(os.path.dirname(__file__))
+
+bruteforce_characters=[' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+                    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', '-',
+                    ',', '!', '?', '=', ')', '(', '/', '&', '%', '*', '0',
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+dictionary_file='.\\dictionary.txt'
+counter=0
+got_result=False
+
+def brute_force_it(char_set):
+    global counter, password, got_result, pdf_file
+    if got_result == True:
+        return
+    while True:
+        for letter in bruteforce_characters:
+            if len(char_set) <= max_password_length:
+                if counter == 0:
+                    char_set = letter
+                else:
+                    char_set+=letter
+                counter+=1
+                check=pdf_file.decrypt(char_set.strip())
+                if check == 1:
+                    got_result=True
+                    password=char_set.strip()
+                    return
+                if len(char_set) < max_password_length:
+                    brute_force_it(char_set)
+                else:
+                    return
+                char_set=char_set[:-1]
+                counter-=1
+            else:
+                return
+        break ### brauchts das?
+
+def dictionary_attack():
+    global pdf_file, password, got_result
+    dict_file_open=open(dictionary_file)
+    dict_content=dict_file_open.read()
+    dict_content=dict_content.split('\n')
+    dict_file_open.close()
+    for possible_password in dict_content:
+        check_1=pdf_file.decrypt(possible_password)
+        check_2=pdf_file.decrypt(possible_password.lower())
+        check_3=pdf_file.decrypt(possible_password.title())
+        if check_1 == 1 or check_2 == 1 or check_3 == 1:
+            if check_1 == 1:
+                password=possible_password
+            elif check_2 == 1:
+                password=possible_password.lower()
+            else:
+                password=possible_password.title()
+            got_result=True
+            return
+
+def choose_password_lenght():
+    global max_password_length
+    print(''.center(70, '█'))
+    print(' Wie lange soll das Passwort höchstens sein? '.center(70, '█'))
+    print(''.center(70, '█'))
+    max_password_length=input(''.center(25))
+    if max_password_length.isdecimal():
+        max_password_length=int(max_password_length)
+        brute_force_it('')
+
+def choose_file():
+    global pdf_file
+    print(''.center(70, '█'))
+    print(' Bitte Namen des PDF-Files angeben (demo.pdf): '.center(70, '█'))
+    print(''.center(70, '█'))
+    file_name=input(''.center(25))
+    if os.path.isfile(file_name) and file_name.endswith('.pdf'):
+        pdf_file=PyPDF2.PdfFileReader(file_name)
+        print(''.center(70, '█'))
+        print(' Passwort wird gesucht... '.center(70, '█'))
+        print(''.center(70, '█'))
+        if attack == 1:
+            choose_password_lenght()
+        else:
+            dictionary_attack()
+
+
+def choose_method():
+    global attack
+    while True:
+        print(''.center(70, '█'))
+        print(' Bitte Methode auswählen '.center(70, '█'))
+        print(''.center(70, '█'))
+        print(''.rjust(15, '█')+'1. Bruteforce-Attack'.center(40)+''.rjust(15, '█'))
+        print(''.rjust(15, '█')+'2. Dictionary-Attack'.center(40)+''.rjust(15, '█'))
+        print(''.center(70, '█'))
+        attack=input(''.rjust(34))
+        if attack.isdecimal():
+            attack=int(attack)
+            if attack < 3 and attack > 0:
+                choose_file()
+        else:
+            continue
+        if got_result == True:
+            show_password()
+        else:
+            no_pass()
+
+def show_password():
+    print(''.center(70, '█'))
+    print(' Passwort gefunden! '.center(70, '█'))
+    print(''.center(70, '█'))
+    print(''.rjust(15, '█')+password.center(40)+''.rjust(15, '█'))
+    print(''.center(70, '█'))
+    input()
+
+def no_pass():
+    print(''.center(70, '█'))
+    print(' Passwort nicht gefunden '.center(70, '█'))
+    print(''.center(70, '█'))
+    input()
+
+choose_method()

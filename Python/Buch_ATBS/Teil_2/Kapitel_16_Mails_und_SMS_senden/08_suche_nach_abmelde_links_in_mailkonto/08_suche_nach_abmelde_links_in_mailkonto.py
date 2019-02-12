e2c8975dff9e90aaf897507ccfcea66aa3427570
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-02-12 23:47:36 +0100

    Update 08_suche_nach_abmelde_links_in_mailkonto.py

--- Python/Buch_ATBS/Teil_2/Kapitel_16_Mails_und_SMS_senden/08_suche_nach_abmelde_links_in_mailkonto/08_suche_nach_abmelde_links_in_mailkonto.py
+++ Python/Buch_ATBS/Teil_2/Kapitel_16_Mails_und_SMS_senden/08_suche_nach_abmelde_links_in_mailkonto/08_suche_nach_abmelde_links_in_mailkonto.py
@@ -33,7 +33,7 @@ def check_mails(uids):
     for uid in uids:
         mail_content=imap_object.fetch(uid, ['BODY[]'])
         raw_mail=pyzmail.PyzMessage.factory(mail_content[uid][b'BODY[]'])
-        # Prüfe ob HTML oder Text-Mail
+        # Prüfe ob HTML oder Text-Mail und decodiere Inhalt
         if raw_mail.html_part != None and raw_mail.html_part.charset != None:
             mail_decoded=raw_mail.html_part.get_payload().decode(raw_mail.html_part.charset)
         elif raw_mail.text_part != None and raw_mail.text_part.charset != None:

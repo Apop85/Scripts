c0f0d95d22fb67caa71af0c84d16d549924707c6
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-02-22 22:15:57 +0100

    Update 01_datentypen.py

--- Python/Buch_Python3_Das_umfassende_Praxisbuch/Kapitel_4_Standarddatentypen/01_datentypen.py
+++ Python/Buch_Python3_Das_umfassende_Praxisbuch/Kapitel_4_Standarddatentypen/01_datentypen.py
@@ -6,7 +6,7 @@
 # Created Date: Friday 22.02.2019, 20:25
 # Author: Apop85
 # -----
-# Last Modified: Friday 22.02.2019, 22:06
+# Last Modified: Friday 22.02.2019, 22:13
 # -----
 # Copyright (c) 2019 Apop85
 # This software is published under the MIT license.
@@ -33,6 +33,7 @@ def output(title, string):
 
 output('Datentypen','Python kennt vier grundliegende Datentypen. Dazu gehören Zahlen, Kollektionen, Boolean-Werte und NoneType.')
 
+print(' Zahlen '.upper().center(max_text_length+9,'█'))
 output('Zahlen','Zu den Zahlen gehören Integer, also Ganzzahlen (1,2,3...), Float, also Fliesskommazahlen (1.123, 4.45...) und complex (1+2j).')
 output('Integertypen','Es gibt noch einige Subtypen von Integern wie folgende: decimalinteger, nonzerodigit, digit, octinteger, hexinteger, bininteger, octdigit, hexdigit und bindigit')
 output('Dezimalzahlen','Dezimalzahlen können ohne prefix als Wert übergeben werden. Z.b. a=10 oder print(15)')
@@ -42,16 +43,18 @@ output('Binärzahlen','Binärzahlen bestehen aus 0 und 1 und werden mit dem Pref
 output('Fliesskommazahlen','Fliesskommazahlen sind Zahlen mit Kommastellen also rationale Zahlen. Für Rationale zahlen die sehr nahe bei Null oder sehr Lang sind verwendet man die exponentielle Schreibweise. Beispielsweise 5e-10 oder 5e10')
 output('Komplexe Zahlen','Komplexe Zahlen bestehen einem realen Part (Ganzzahl oder Fliesskommazahl) und einem immaginären Part (j oder J)')
 
+print(' Kollektionen '.upper().center(max_text_length+10,'█'))
 output('Kollektionen','Kollektionen werden in drei Unterkategorien unterteilt. Sequenzen, Mengen und Abbildungen.')
-
 output('Sequenzen','Sequenzen werden wiederum in veränderbar und unveränderbar unterteilt. Zu den Unveränderbaren gehören: String, Bytestring und Tuple. Zu den Veränderbaren gehören: Listen und Bytearrays.')
 output('Eigenschaften von Sequenzen','Was jedoch alle Sequenzen gemeinsam haben ist dass sie einen Index für die enthaltenen Werte besitzt. Wobei das erste Listenelement immer dem Index 0 entspricht. Somit ist das Letzte Item mit n-1 aufrufbar.')
-
 output('Mengen','Als Mengen bezeichnet man eine untergeordnete Kollektion von Objekten, wobei jedes Objekt nur einmal vorkommen darf. Dazu gehören set und frozenset')
 output('Abbildungen','Die einzige Datentyp von Python der als Abbildung bezeichnet wird ist das Dictionary.')
 
+print(' Boolean '.upper().center(max_text_length+10,'█'))
 output('Boolean-Werte','Ein Boolean-Wert kann nur entweder True oder False beinhalten. Leere Dictionarys, Listen, die Zahl Null und unwahre logische Aussagen besitzen den Wahrheitswert False. Alle nicht leeren Sequenzen oder Dictionarys, wahre logische Aussagen und alle Zahlen ungleich Null besitzen den Wahrheitswert True.')
 
+print(' NoneType '.upper().center(max_text_length+10,'█'))
 output('NoneType','Der NoneType besitzt im Gegensatz zu den Anderen Datentypen keinen Wert. Er ist und bleibt None und gehört zu den literalen Datentypten.')
 
-output('Unterschied Typ - Klasse','Der Datentyp bezeichnet nur wie das Objekt verarbeitet werden soll. Die Klasse ist ein Bauplan für gleichartige Objekte. In der Klassendefinition wird festgelegt welche Attribute und Methoden die Objekte der Klasse besitzen.')
+print(' '. center(max_text_length+10,'█'))
+output('Unterschied: Typ - Klasse','Der Datentyp bezeichnet nur wie das Objekt verarbeitet werden soll. Die Klasse ist ein Bauplan für gleichartige Objekte. In der Klassendefinition wird festgelegt welche Attribute und Methoden die Objekte der Klasse besitzen.')

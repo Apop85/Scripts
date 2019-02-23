a8f5076be2aa9cc460adbb5471e5f664b53c0b15
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-02-23 22:26:16 +0100

    Create 03_kapitel_05_repetitionsaufgabe_02_altersprüfung.py

---
+++ Python/Buch_Python3_Das_umfassende_Praxisbuch/Kapitel_05_Kontrollstrukturen/03_kapitel_05_repetitionsaufgabe_02_altersprüfung.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# File: 03_kapitel_05_repetitionsaufgabe_02_altersprüfung.py
+# Project: Kapitel_05_Kontrollstrukturen
+# Created Date: Saturday 23.02.2019, 22:20
+# Author: Apop85
+# -----
+# Last Modified: Saturday 23.02.2019, 22:26
+# -----
+# Copyright (c) 2019 Apop85
+# This software is published under the MIT license.
+# Check http://www.opensource.org/licenses/MIT for further informations
+# -----
+# Description: Task 2. Page 144. This script will check the user input and determine 
+# if the user is a youth or mature.
+###
+
+while True:
+    user_age=input('Please input your age: ')
+    if user_age.isdecimal() and int(user_age) > 0:
+        user_age=int(user_age)
+        break
+
+if user_age < 18:
+    print('You\'re a youth.')
+else:
+    print('You\'re mature.')

3b7386171729a2b4c242191059f984de5d3a1aa8
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-03-01 01:02:39 +0100

    Update collatz_numbers.py

--- Python/Sonstige_Uebungen/collatz_numbers.py
+++ Python/Sonstige_Uebungen/collatz_numbers.py
@@ -6,7 +6,7 @@
 # Created Date: Thursday 28.02.2019, 21:06
 # Author: Apop85
 # -----
-# Last Modified: Thursday 28.02.2019, 21:58
+# Last Modified: Thursday 28.02.2019, 23:24
 # -----
 # Copyright (c) 2019 Apop85
 # This software is published under the MIT license.
@@ -16,9 +16,12 @@
 ###
 
 
-def collatz(n):
-    highscore, length=[0,0], 0
-    for i in range(10**n,10**(n+1)):
+def collatz(n1=1,n2=10, step=10000, length=0):
+    if step < 1:
+        return
+    highscore=[0,0]
+    values=[]
+    for i in range(n1,n2,step):
         counter=0
         n=i
         while n != 1:
@@ -27,6 +30,7 @@ def collatz(n):
             else:
                 n=n*3+1
             counter+=1
+        values+=[(counter,i)]
         if counter > highscore[1]:
             if length > 0:
                 print('\r'*(length+10), end='')
@@ -34,9 +38,13 @@ def collatz(n):
             string='Current highscore: '+str(highscore[0])+' with '+str(highscore[1])+' calculations.'
             length=len(string)
             print(string, end='')
+    values.sort()
+    collatz(n1=values[len(values)//2][1]//2, n2=n2, step=step//10, length=length)
     print('\r'*(length+10), end='')
-            
+
     print('Highscore: '+str(highscore[0])+' with '+str(highscore[1])+' calculations.'+' '*50)
+    # values.sort()
+    # print(values[-2:])
 
 def input_number():
     while True:
@@ -44,6 +52,10 @@ def input_number():
         if factorial.isdecimal():
             return int(factorial)
 
+def prepare_collatz(n):
+    return 10**n, 10**(n+1)
+
 while True:
     n=input_number()
-    collatz(n)
+    n1,n2=prepare_collatz(n)
+    collatz(n1,n2,step=10**(len(str(n1))-2))

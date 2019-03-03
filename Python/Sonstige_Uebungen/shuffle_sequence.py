eb3d5bbc2d6a3a8a4aa0a75f59bc50f3050c040e
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-03-03 16:35:54 +0100

    Update shuffle_sequence.py

--- Python/Sonstige_Uebungen/shuffle_sequence.py
+++ Python/Sonstige_Uebungen/shuffle_sequence.py
@@ -6,7 +6,7 @@
 # Created Date: Saturday 02.03.2019, 06:01
 # Author: Apop85
 # -----
-# Last Modified: Sunday 03.03.2019, 12:58
+# Last Modified: Sunday 03.03.2019, 16:32
 # -----
 # Copyright (c) 2019 Apop85
 # This software is published under the MIT license.
@@ -15,25 +15,26 @@
 # Description: Attempt to improve the enigma script
 ###
 original_len=0
+d_counter=0
+z=0
 def shuffle(a=5,b=5,c=5,alphabet='abcdefghijklmnopqrstuvwxyz'):
     from math import factorial
     a,b,c=a%(len(alphabet)),b%(len(alphabet)),c%(len(alphabet))
     alpha_factorial=factorial(len(alphabet))
-    global d_counter, original_len
+    global d_counter, original_len, z
     liste=[]
-    d_counter=0
     i,j,k=0,0,0
     alphabet=list(alphabet)
     if original_len == 0:
         original_len=len(alphabet)
-    print('Iteration'.center(15)+'|'+'Duplicates'.center(15)+'|'+'Fail-rate'.center(15)+'|'+'Generation'.center(15)+'|'+'Done'.center(15))
+    print('Iteration'.center(12)+'|'+'Duplicates'.center(12)+'|'+'Fail-rate'.center(12)+'|'+'Generation'.center(12)+'|'+'Done'.center(12))
     for z in range(alpha_factorial):
         check_alph=''.join(alphabet)
         if check_alph in liste:
             d_counter+=1
         else:
             liste+=[check_alph]
-        string=str(z+1).center(15)+'|'+str(d_counter).center(15)+'|'+(str(round(100/(z+1)*d_counter,7))+'%').center(15)+'|'+(str(i)+'-'+str(j)+'-'+str(k)).center(15)+'|'+(str(round(100/(len(alphabet)**3)*z))+'%').center(15)+'   '
+        string=str(z+1).center(12)+'|'+str(d_counter).center(12)+'|'+(str(round(100/(z+1)*d_counter,7))+'%').center(12)+'|'+(str(i)+'-'+str(j)+'-'+str(k)).center(12)+'|'+(str(round(100/(len(alphabet)**3)*z))+'%').center(12)+'   '
         print('\r'*len(string)+string, end=' ')
         k+=1
         if k == len(alphabet):
@@ -49,17 +50,24 @@ def shuffle(a=5,b=5,c=5,alphabet='abcdefghijklmnopqrstuvwxyz'):
         alphabet.insert((i-1),p1)
         p2=alphabet[j]
         del alphabet[j]
-        alphabet.insert(-1,p2)
+        alphabet.insert(-1+j,p2)
         p3=alphabet[k]
         del alphabet[k]
         alphabet.insert((k-5),p3)
         if len(alphabet) != original_len:
-            raise Exception('AlgorythmError: Algorythm changes length of alphabet.')
+            raise Exception('AlgorithmError: Algorythm changes length of alphabet.')
+        if d_counter > 0:
+            raise Exception('AlgorithmError: Algorithm failed at iteration '+str(z+1)+'. Done '+str(round(100/alpha_factorial*(z+1),5))+'% of all possible variations.')
         if (i,j,k) == (a,b,c):
+        # if not True:
             print()
             yield alphabet
+        
 
-
-alphabet=shuffle(74,74,74,alphabet='abcdefghijklmnopqrstuvwxyzäöü ?!,.0123456789"\'/\\@éàèç€%_-=¢$|¬§°#´^<>+*)(;:')
-print(''.join(next(alphabet)))
-print('Duplicates: '+str(d_counter)+' Total: '+str(n_counter))
+shuffle(74,74,74,alphabet='abcdefghijklmnopqrstuvwxyzäöü ?!,.0123456789"\'/\\@éàèç€%_-=¢$|¬§°#´^<>+*)(;:')
+# alphabet=shuffle(25,25,25,alphabet='abcdefghijklmnopqrstuvwxyz')
+# alphabet=shuffle(11,11,11,alphabet='0123456789abcdefgh')
+# alphabet=shuffle(alphabet='0123456789')
+# print(''.join(next(alphabet)))
+print('Duplicates: '+str(d_counter)+' Total: '+str(z+1))
+# input('Enter to exit...')

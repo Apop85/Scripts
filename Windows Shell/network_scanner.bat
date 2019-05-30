37e6867b7539271fee68405cb4868d423b71f135
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-05-30 22:42:55 +0200

    Update network_scanner.bat

--- Windows Shell/network_scanner.bat
+++ Windows Shell/network_scanner.bat
@@ -4,19 +4,19 @@ rem Simple network scanner for Windows shell
 
 rem Set IP-Range
 rem --------------------------------------------------------------------
-echo Set base address (xxx.0.0.1): 
+echo Set base address (XXX.0.0.1): 
 set /p input=
 set /a address=input
 
-echo Set base address (1.xxx.0.1):
+echo Set base address (1.XXX.0.1):
 set /p input=
 set /a  address2=input
 
-echo Set subnet range(123.45.0-y.1):
+echo Set subnet range(123.45.0-X.1):
 set /p input=
 set /a subnet=input
 
-echo Set IP range (123.45.6.1-z):
+echo Set IP range (123.45.6.1-X):
 set /p input=
 set /a ip=input
 echo %ip%

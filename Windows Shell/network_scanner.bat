eaf4f16c23392ce30896a9fb1641b058fdc4a0e7
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-05-30 22:41:28 +0200

    Update network_scanner.bat

--- Windows Shell/network_scanner.bat
+++ Windows Shell/network_scanner.bat
@@ -1,23 +1,29 @@
 @echo off
+setlocal enabledelayedexpansion
 rem Simple network scanner for Windows shell
 
 rem Set IP-Range
 rem --------------------------------------------------------------------
-echo Set base adress (xxx.0.0.1): 
-set /p adress=
+echo Set base address (xxx.0.0.1): 
+set /p input=
+set /a address=input
 
-echo Set base adress (1.xxx.0.1):
-set /p adress2=
-set /a adress2=%adress2%
+echo Set base address (1.xxx.0.1):
+set /p input=
+set /a  address2=input
 
 echo Set subnet range(123.45.0-y.1):
-set /p subnet=
-set /a subnet=%subnet&%
+set /p input=
+set /a subnet=input
 
 echo Set IP range (123.45.6.1-z):
-set /p ip=
-set /a ip=%ip%
+set /p input=
+set /a ip=input
+echo %ip%
 
 rem ping defined ip range and output if answer = true
 rem --------------------------------------------------------------------
-for /l %y in (0,1,%subnet%) do for /l %x in (1,1,%ip%) do ping -n 1 %adress%.%adress2%.%y.%x | find ": Bytes" && nslookup %adress%.%adress2%.%y.%x
+cls
+for /l %%y in (0,1,%subnet%) do for /l %%x in (1,1,%ip%) do ping -n 1 %address%.%address2%.%%y.%%x | find ": Bytes" && nslookup %address%.%address2%.%%y.%%x && echo %address%.%address2%.%%y.%%x >> %UserProfile%\Desktop\output_new.txt
+cls
+echo. && echo. && echo. && echo Scan done!

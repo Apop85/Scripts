aaed9dacc9c291ee20b7d53f8f6b9ff7562af89f
Apop85 <39341618+Apop85@users.noreply.github.com>
2019-06-02 19:23:39 +0200

    Update pdf_ocr.py

--- Python/Sonstige_Uebungen/pdf_ocr/pdf_ocr.py
+++ Python/Sonstige_Uebungen/pdf_ocr/pdf_ocr.py
@@ -6,7 +6,7 @@
 # Created Date: Sunday 02.06.2019, 17:30
 # Author: Apop85
 # -----
-# Last Modified: Sunday 02.06.2019, 19:12
+# Last Modified: Sunday 02.06.2019, 19:23
 # -----
 # Copyright (c) 2019 Apop85
 # This software is published under the MIT license.
@@ -27,43 +27,50 @@ from pdf2image.exceptions import (
     PDFSyntaxError
 )
 
-# Set target
+# Set target directories
 target_dir_img = r'.\images'
 target_dir_pdf = r'.\pdf'
 target_dir_txt = r'.\txt'
+tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 def init_script():
-    tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+    # Initialize conditions
     pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
     os.chdir(os.path.dirname(__file__))
 
 def get_filenames(dirname):
+    # Get a list of all files in target dir
     file_list=os.listdir(dirname)
     return file_list
 
 def open_pdf(file_list):
+    # Open all pdf files and read out text
     global pdf_file
     for pdf_file in file_list:
         pdf2img(pdf_file)
 
 def pdf2img(pdf_file):
+    # Convert PDF into Images
     global target_dir_pdf, target_dir_img
     images_from_path = convert_from_path(target_dir_pdf+'\\'+pdf_file,  output_folder=target_dir_img, fmt='jpg')
     img2text(images_from_path)
 
 def img2text(images):
+    # Read out text from images 
     text=''
     for image in images:
         text += pytesseract.image_to_string(image)
     write_txt_file(text)
 
 def write_txt_file(text):
+    # Write resulting file content to txt file
     text_file=open(target_dir_txt+'\\'+pdf_file[:-3]+'txt', 'w')
     text_file.write(text)
     text_file.close()
     clear_tmp_images()
 
 def clear_tmp_images():
+    # Remove all images
     image_list=os.listdir(target_dir_img)
     for image in image_list:
         os.remove(target_dir_img+'\\'+image)

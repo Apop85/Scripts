217c3db6f4cf06a1918fbcecd43631a6e18f167c
Apop85 <rbaldinger@gmx.ch>
2019-08-07 11:33:49 +0200

    PHP Übung (Webseite)

---
+++ PHP/001_Erste_Webseite/index.php
@@ -0,0 +1,6 @@
+<?php
+    $the_title = "Home";
+    $the_content = "<p>This is the Homepage</p>";
+?>
+
+<?php include("php/single.php"); ?>
---
+++ PHP/001_Erste_Webseite/php/sidebar.php
@@ -0,0 +1,4 @@
+            <div id="sidebar">
+                <h3>Sidebar Header</h3>
+                <P>This is the sidebar area</p>
+            </div>

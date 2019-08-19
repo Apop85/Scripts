4321447a78aa053d023a2f433d402e2046cbf833
Apop85 <rbaldinger@gmx.ch>
2019-08-19 22:27:41 +0200

    Bugfix
    Editieren von Lernkarten speichert nun wieder erfolgreich die geänderten Inhalte ab

--- PHP/002_Lernkarten/py/edit.py
+++ PHP/002_Lernkarten/py/edit.py
@@ -8,7 +8,7 @@
 # Created Date: Friday 09.08.2019, 12:48
 # Author: rbald
 #-----
-# Last Modified: Tuesday 13.08.2019, 00:26
+# Last Modified: Monday 19.08.2019, 22:25
 #-----
 # Copyright (c) 2019 rbald
 # This software is published under the MIT license.
@@ -19,6 +19,8 @@
 
 from sys import argv
 import os, re
+
+# argv = ["yy", "edit", "./cards/304 grundlagen/047274c9af7e4836e54d58a515dd4fbe.php", "Was bedeutet RJ45?", "Registered Jack 45!"]
 
 def get_root():
     os.chdir(os.path.dirname(__file__))
@@ -51,24 +53,24 @@ if argv[1] == "edit" and argv[2] != "" and argv[3] != "" and argv[4] != "":
         file_pattern = re.compile(r'\$f = (.*);')
         score_pattern = re.compile(r'\$s = (.*);')
         for line in file_content:
-            if "$q" in line:
+            if "$q =" in line:
                 question = question_pattern.findall(line)
-            elif "$a" in line:
+            elif "$a =" in line:
                 answer = answer_pattern.findall(line)
-            elif "$f" in line:
+            elif "$f =" in line:
                 file_path = file_pattern.findall(line)
-            elif "$s" in line:
+            elif "$s =" in line:
                 score = score_pattern.findall(line)
 
         question, answer, file_path, score = question[0], answer[0], file_path[0], score[0]
-        output = "<?php\n\t$q = '{}';\n\t$a = '{}';\n\t$f = {};\n\t$s = {};\n?>".format(frage, antwort, file_path, score)
+        output = "<?php\n\t$q = '{}';\n\t$a = '{}';\n\t$f = {};\n\t$s = {};\n\t$ra = 0;\n\t$fa = 0;\n?>".format(frage, antwort, file_path, score)
         
         try:
             file_handler = open(abs_path, "w", encoding="utf-8")
             file_handler.write(output)
             file_handler.close()
         except:
-            print("<div class='output_message error'>Fehler bei der Editierung</div>")    
+            print("<div class='output_message error'>Fehler bei der Editierung</div>")
         print("<div class='output_message info'>Gespeichert</div>")
     else:
         print("<div class='output_message error'>Datei nicht gefunden</div>")

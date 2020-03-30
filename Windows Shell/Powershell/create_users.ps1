49cc0679ac3b8daf285fce75ad57463f66026020
Apop85 <rbaldinger@gmx.ch>
2020-03-30 12:12:38 +0200

    Update create_users.ps1

--- Windows Shell/Powershell/create_users.ps1
+++ Windows Shell/Powershell/create_users.ps1
@@ -1,3 +1,19 @@
+####
+# File: create_users.ps1
+# Project: Modul 159
+#-----
+# Created Date: Monday 30.03.2020, 09:25
+# Author: Apop85
+#-----
+# Last Modified: Mon Mar 30 2020
+#-----
+# Copyright (c) 2020 Apop85
+# This software is published under the MIT license.
+# Check http://www.opensource.org/licenses/MIT for further informations
+#-----
+# Description: Erstellen von AD-Benutzern und Gruppen
+####
+
 # Benutzerdefinitionen
 $usernames = @{ 'ursmei01' = 'Urs', 'Meier', 'urs.meier@creasol.ch', 'CEO'; 
                 'viokra01' = 'Viola', 'Krätzer', 'viola.kraezer@creasol.ch', 'Marketing'; 
@@ -19,26 +35,32 @@ $company = "creasol"
 $city = "Zürich"
 
 # Erstelle Gruppen
+$counter = 0
 foreach ($group in $user_groups) {
     if (@(Get-ADGroup -Filter { SamAccountName -eq $group }).Count -eq 0) {
+        # Existiert die Gruppe noch nicht?
         Write-Warning "Group $group does not exist."
         Write-Host "Creating group $group"
+        $counter += 1
         # Erstelle Gruppe
         New-ADGroup -Name $group -Path "CN=Users, OU=Groups, OU=creasol ,DC=zh, DC=ch, DC=creasol, DC=ch" -GroupScope "Global"
     }
 }
 
 Write-Host "##########################"
-Write-Host "Groups have been added."
+Write-Host "$counter Groups have been added."
 Write-Host "##########################"
 
 # Erstelle User
 foreach ($user in $user_keys) {
     # Definiere Departpent
     $department = $usernames.Get_Item($user)[3]
+    $counter = 0
     if (@(Get-ADUser -Filter { SamAccountName -eq $user }).Count -eq 0) {
+        # Existiert der User noch nicht?
         Write-Warning -Message "User $user does not exist."
         Write-Host "Creating user: $user"
+        $counter += 1
         # Lese Userdetails aus Hashtable aus
         $vorname = $usernames.Get_Item($user)[0]
         $nachname = $usernames.Get_Item($user)[1]
@@ -51,6 +73,7 @@ foreach ($user in $user_keys) {
         # Benutzer zu Gruppe hinzufügen
         Add-ADGroupMember -Identity $department -Members $user
     } elseif ($members -notcontains $user) {
+        # Ist der aktuelle User NICHT in der vorgesehenen Gruppe?
         Write-Warning "User $user already exists but is not in group $group."
         # Benutzer zu Grupper hinzufügen
         Add-ADGroupMember -Identity $department -Members $user
@@ -59,5 +82,5 @@ foreach ($user in $user_keys) {
 }
 
 Write-Host "##########################"
-Write-Host "Users have been added."
+Write-Host "$counter Users have been added."
 Write-Host "##########################"
